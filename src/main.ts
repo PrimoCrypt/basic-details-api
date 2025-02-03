099255bb1c306cdc5c3662ba98fec62c1978a5a1
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2025-02-03 13:01:42 +0100

    feat: added cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://basic-details-api.onrender.com/',
+    ], // Add your allowed origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
