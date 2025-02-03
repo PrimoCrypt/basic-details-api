83d73403aabc21ba2ac5d9000e0514b15851e264
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2025-02-03 12:26:14 +0100

    fix: updated the github link to the accurate one

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ export class AppService {
     return JSON.stringify({
       email: 'yhungdew@gmail.com',
       current_datetime: new Date().toISOString(),
-      github_url: 'https://github.com/yhungdew',
+      github_url: 'https://github.com/PrimoCrypt/basic-details-api',
     });
   }
 }
